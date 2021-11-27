5cee57b4dcfba160c7baaeaa5c42931227017fdd
Rendo Ruiz <rendo.ruiz@outlook.com>
2021-11-27 14:09:52 -0700

    lighten skeleton loader color intensity

--- components/Story/StoryItem/StoryItemWrapper.js
+++ components/Story/StoryItem/StoryItemWrapper.js
@@ -50,12 +50,12 @@ const IsLoading = () => {
   return (
     <div className="grid grid-cols-[40px,1fr] break-words overflow-hidden border-brandDefault border-brandBorder rounded">
       <div className="flex justify-center items-start py-2 bg-white/80">
-        <div className="rounded-md w-3/4 h-3 bg-brandTextSecondary/80 animate-pulse"></div>
+        <div className="rounded-md w-3/4 h-3 bg-brandTextSecondary/30 animate-pulse"></div>
       </div>
       <div className="grid gap-3 items-start bg-white p-2">
-        <div className="rounded-md w-5/12 h-4 bg-brandTextSecondary/80 animate-pulse"></div>
-        <div className="rounded-md w-10/12 h-5 bg-brandTextSecondary/80 animate-pulse"></div>
-        <div className="rounded-md w-7/12 h-5 bg-brandTextSecondary/80 animate-pulse"></div>
+        <div className="rounded-md w-5/12 h-4 bg-brandTextSecondary/30 animate-pulse"></div>
+        <div className="rounded-md w-10/12 h-5 bg-brandTextSecondary/30 animate-pulse"></div>
+        <div className="rounded-md w-7/12 h-5 bg-brandTextSecondary/30 animate-pulse"></div>
       </div>
     </div>
   );
