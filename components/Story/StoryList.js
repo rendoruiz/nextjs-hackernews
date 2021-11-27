1d8ad7d2da6aad6d45d142c9b8f07179b0b03a5e
Rendo Ruiz <rendo.ruiz@outlook.com>
2021-11-27 14:06:01 -0700

    error message style alignment

--- components/Story/StoryList.js
+++ components/Story/StoryList.js
@@ -21,9 +21,9 @@ const StoryList = ({ useHook, itemLimit = 20 }) => {
 const IsLoading = () => {
   return (
     <div className="grid place-items-center">
-      <p className="grid gap-5 justify-items-center">
-        <div className="animate-spin rounded-full w-10 h-10 border-b-4 border-brandOrange" />
-        <span className="font-medium text-xl">Fetching content ...</span>
+      <p className="grid gap-5 justify-items-center sm:gap-8">
+        <div className="animate-spin rounded-full w-10 h-10 border-b-4 border-brandOrange sm:w-14 sm:h-14" />
+        <span className="font-medium text-2xl sm:text-3xl">Fetching content ...</span>
       </p>
     </div>
   )
