e9d4202f496115bb6bf08d9d2296025332ff62ae
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-02 20:08:27 -0700

    style alignment

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -4,10 +4,10 @@ import AppIconGlyph from "./Glyphs/AppIconGlyph";
 
 const SiteHeader = () => {
   return ( 
-    <header className="sticky top-0 z-30 flex items-center px-4 py-2 bg-[#1d2535] text-white shadow-lg sm:bg-white sm:text-brandTextPrimary">
+    <header className="sticky top-0 z-30 flex items-center px-4 py-[0.625rem] bg-[#1d2535] text-white shadow-lg sm:bg-white sm:text-brandTextPrimary">
       <Link href="/">
         <a className="flex items-center">
-          <AppIconGlyph className=" w-7 h-7 text-brandOrange"/>
+          <AppIconGlyph className="w-7 h-7 text-brandOrange"/>
           <h1 className="ml-2 font-primary font-bold text-lg">Hacker News</h1>
         </a>
       </Link>
