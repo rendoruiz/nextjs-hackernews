5187eb18a098141e14951c3698c5cb599ac4ab78
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-08 14:46:52 -0700

    comment header desktop

--- components/Comment/CommentItem/CommentItemHeader.js
+++ components/Comment/CommentItem/CommentItemHeader.js
@@ -6,16 +6,18 @@ import CommentItemMobileOverflowModal from "./CommentItemMobileOverflowModal";
 import UserAvatar from "../../User/UserAvatar";
 import UserLink from "../../User/UserLink";
 import MicrophoneGlyph from "../../Glyphs/MicrophoneGlyph";
+import UserHoverCard from "../../User/UserHoverCard";
+import TimeTooltip from "../../Shared/TimeTooltip";
 
 const CommentItemHeader = ({ commentData, submitterId, itemDepth, isDead = false, toggleDisplayState }) => {
   return (  
     <div className={clsx(
-      "grid grid-cols-[auto,1fr] gap-2 text-brandTextPrimary",
+      "grid grid-cols-[auto,1fr] gap-2 text-brandTextPrimary sm:tracking-wide",
       { "opacity-60": isDead }
     )}>
       <UserAvatar 
         userId={commentData.by} 
-        className={itemDepth === 0 ? "w-6 h-6" : "w-[18px] h-[18px]"}
+        className={clsx(itemDepth === 0 ? "w-6 h-6" : "w-[18px] h-[18px]", "sm:hidden")}
       />
       
       <div className="grid grid-cols-[auto,1fr,auto] items-center text-xs">
@@ -23,7 +25,16 @@ const CommentItemHeader = ({ commentData, submitterId, itemDepth, isDead = false
           "flex font-bold sm:font-medium", 
           { "text-brandOrange sm:text-brandTextPrimary": commentData.by === submitterId }
         )}>
-          <UserLink userId={commentData.by} />
+          <UserLink 
+            userId={commentData.by} 
+            className="sm:hidden"
+          />
+          <UserHoverCard 
+            userId={commentData.by} 
+            withAvatar
+            className="hidden sm:flex items-center"
+            avatarClassName="mr-2 w-7 h-7"
+          />
           
           { commentData.by === submitterId && (
             <>
@@ -51,6 +62,7 @@ const CommentItemHeader = ({ commentData, submitterId, itemDepth, isDead = false
           )}
         </div>
 
+        {/* mobile time + collapse toggle */}
         <button 
           className="text-left sm:hidden"
           onClick={(e) => toggleDisplayState(e)}
@@ -58,6 +70,16 @@ const CommentItemHeader = ({ commentData, submitterId, itemDepth, isDead = false
           &nbsp;&nbsp;
           { useShortRelativeTime(commentData.time) }
         </button>
+
+        {/* desktop time tooltip */}
+        <div className="hidden sm:block text-brandTextSecondary">
+          &nbsp;•&nbsp;
+          <TimeTooltip
+            unixTime={commentData.time}
+            contentId={commentData.id}
+            isComment
+          />
+        </div>
 
         <CommentItemMobileOverflowModal commentData={commentData} />
       </div>
