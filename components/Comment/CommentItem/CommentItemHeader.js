79d8cb05f5f9ff8ba2f4f9cdbb0ec41056920eed
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-08 13:32:07 -0700

    original poster indicator, align tooltips

--- components/Comment/CommentItem/CommentItemHeader.js
+++ components/Comment/CommentItem/CommentItemHeader.js
@@ -1,7 +1,11 @@
+import clsx from "clsx";
+
 import { useShortRelativeTime } from "../../../hooks/useDate";
 import CommentItemMobileOverflowModal from "./CommentItemMobileOverflowModal";
 import UserAvatar from "../../User/UserAvatar";
 import UserLink from "../../User/UserLink";
+import MicrophoneGlyph from "../../Glyphs/MicrophoneGlyph";
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip";
 
 const CommentItemHeader = ({ commentData, submitterId, itemDepth, toggleDisplayState }) => {
   return (  
@@ -12,14 +16,38 @@ const CommentItemHeader = ({ commentData, submitterId, itemDepth, toggleDisplayS
       />
       
       <div className="grid grid-cols-[auto,1fr,auto] items-center text-xs">
-        <div className="flex font-medium">
+        <div className={clsx(
+          "flex font-bold sm:font-medium sm:text-brandTextPrimary", 
+          { "text-brandOrange": commentData.by !== submitterId }
+        )}>
           <UserLink 
             userId={commentData.by} 
             className=""
           />
           
-          { commentData.by !== submitterId && (
-            <span>OP</span>
+          { commentData.by === submitterId && (
+            <>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <span className="hidden sm:block font-bold text-brandOrange cursor-default">
+                    &nbsp;OP
+                  </span>
+                </TooltipTrigger>
+                <TooltipContent
+                  className="rounded px-2 py-[2px] bg-black font-medium text-xs text-white"
+                  side="top"
+                  sideOffset={4}
+                >
+                  <span>Original Poster</span>
+                  <TooltipArrow className="my-[-0.5px] text-black fill-current" />
+                </TooltipContent>
+              </Tooltip>
+              
+              <MicrophoneGlyph 
+                className="ml-1 w-4 h-4 text-brandOrange sm:hidden" 
+                title="original poster indicator"
+              />
+            </>
           )}
         </div>
 
