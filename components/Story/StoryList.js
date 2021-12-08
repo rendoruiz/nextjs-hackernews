9f734a6e410d6de7ffdce19fc33f1bd6a9eaa7fa
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-07 18:16:59 -0700

    refactor, align code with commentlist

--- components/Story/StoryList.js
+++ components/Story/StoryList.js
@@ -1,21 +1,55 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/dist/client/router";
+
+import { useCountQueryString } from "../../hooks/useCountQueryString";
 import IsError from "../StatusMessage/IsError";
 import IsLoading from "../StatusMessage/IsLoading";
 import StoryItemWrapper from "./StoryItem/StoryItemWrapper";
 
-const StoryList = ({ useHook, itemLimit }) => {
+const StoryList = ({ useHook }) => {
+  const router = useRouter();
   const { isLoading, isError, data, isSuccess } = useHook;
+  const [itemCount, setItemCount] = useState(null);  // todo: save state locally
+  const defaultCount = 25; 
 
-  return !itemLimit || isLoading ? (<IsLoading />) : isError ? (<IsError />) : isSuccess && (  
-    <div className="grid content-start gap-1 sm:gap-3">
-    {
-      [...data].slice(0, itemLimit).map((storyId) => 
-        <StoryItemWrapper
-          key={storyId}
-          storyId={storyId}
-        />
-      )
-    }
-    </div>
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newCount = itemCount + defaultCount;
+    setItemCount(newCount);
+    router.push({
+      pathname: router.pathname,
+      query: { count: newCount },
+    }, undefined, { shallow: true });
+  }
+
+  useEffect(() => {
+    setItemCount(useCountQueryString(defaultCount));
+  }, []);
+
+  return isLoading ? (<IsLoading />) : isError ? (<IsError />) : isSuccess && (  
+    <>
+      <div className="grid content-start gap-1 sm:gap-3">
+        {
+          [...data].slice(0, itemCount).map((storyId) => 
+            <StoryItemWrapper
+              key={storyId}
+              storyId={storyId}
+            />
+          )
+        }
+      </div>
+
+      { itemCount && (
+        <div className="grid mt-1 px-4 py-[0.625rem] bg-white sm:place-items-center sm:bg-transparent sm:mt-2 sm:pb-0">
+          <button 
+            className="rounded-full px-10 py-[0.375rem] bg-brandOrange font-medium text-sm text-white transition-opacity hover:opacity-80 active:opacity-60"
+            onClick={(e) => handleClick(e)}
+          >
+            View More Stories
+          </button>
+        </div>
+      )}
+    </>
   );
 }
  
--- components/Story/StoryView.js
+++ components/Story/StoryView.js
@@ -1,30 +1,8 @@
-import { useRouter } from "next/dist/client/router";
-import { useEffect, useState } from "react";
-
-import { useCountQueryString } from "../../hooks/useCountQueryString";
 import SiteLayout from "../SiteLayout";
 import StoryNav from "./StoryNav";
 import StoryList from "./StoryList";
 
 const StoryView = ({ useHook, activeRoute }) => {
-  const router = useRouter();
-  // todo: local state management & retain scroll position when navigating back
-  const [itemCount, setItemCount] = useState(null);  
-  const defaultCount = 25; 
-
-  useEffect(() => {
-    setItemCount(useCountQueryString(defaultCount));
-  }, []);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    const newCount = itemCount + defaultCount;
-    setItemCount(newCount);
-    router.push({
-      pathname: router.pathname,
-      query: { count: newCount },
-    }, undefined, { shallow: true });
-  }
 
   return ( 
     <SiteLayout contentClassName="grid-rows-[auto,1fr,auto]">
@@ -32,21 +10,7 @@ const StoryView = ({ useHook, activeRoute }) => {
       <StoryNav activeRoute={activeRoute} />
 
       {/* Stories */}
-      <StoryList 
-        useHook={useHook} 
-        itemLimit={itemCount}
-      />
-
-      { itemCount && (
-        <div className="grid mt-1 px-4 py-[0.625rem] bg-white sm:place-items-center sm:bg-transparent sm:mt-2 sm:pb-0">
-          <button 
-            className="rounded-full px-10 py-[0.375rem] bg-brandOrange font-medium text-sm text-white transition-opacity hover:opacity-80 active:opacity-60"
-            onClick={(e) => handleClick(e)}
-          >
-            View More Stories
-          </button>
-        </div>
-      )}
+      <StoryList useHook={useHook} />
     </SiteLayout>
   );
 }
