08f23325a21d1dcfe38c15e02cda9e87e2a0e86d
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-08 12:42:50 -0700

    desktop no comment indicator

--- components/Comment/CommentList.js
+++ components/Comment/CommentList.js
@@ -18,24 +18,37 @@ const CommentList = ({ storyId }) => {
 
   return isLoading ? (<IsLoading />) : isError || !storyData ? (<IsError />) : isSuccess && (  
     <div className="grid content-start gap-3 pt-1 pb-3 bg-white sm:border-brandDefault sm:border-brandBorder sm:rounded sm:p-3 sm:shadow-sm">
+      {/* mobile comment count (displayed by default) */}
       <div className="px-4 sm:hidden">
-        <span className="font-bold text-xs">
+        <span className="font-bold text-xs sm:hidden">
           { storyData.descendants === 0 ? "No" : storyData.descendants } Comment{ storyData.descendants !== 1 && "s" }
         </span>
       </div>
 
-      <div className="group grid content-start gap-5">
-        { storyData.kids &&
-          [...storyData.kids].slice(0, isCountLimited ? defaultCount : storyData.kids.length).map((commentId) => (
-            <CommentItemWrapper
-              key={commentId}
-              commentId={commentId}
-              submitterId={storyData.by}
-              replyDepthLimit={replyDepthLimit}
-            />
-          ))
-        }
-      </div>
+      {/* desktop comment count (only displayed if there are no comments) */}
+      { storyData.descendants === 0 && (
+        <div className="hidden sm:block">
+          <span className="font-medium">
+            No Comments
+          </span>
+        </div>
+      )}
+      
+      {/* comment list */}
+      { storyData.kids && (
+        <div className="group grid content-start gap-5">
+          {
+            [...storyData.kids].slice(0, isCountLimited ? defaultCount : storyData.kids.length).map((commentId) => (
+              <CommentItemWrapper
+                key={commentId}
+                commentId={commentId}
+                submitterId={storyData.by}
+                replyDepthLimit={replyDepthLimit}
+              />
+            ))
+          }
+        </div>
+      )}
 
       {/* load more comments trigger */}
       { isCountLimited && storyData.kids && storyData.kids.length > defaultCount && (
