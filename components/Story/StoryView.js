96af5d4c40721b0a78af2a3a7c68b36859609333
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-07 18:19:08 -0700

    Update StoryView.js

--- components/Story/StoryView.js
+++ components/Story/StoryView.js
@@ -2,7 +2,7 @@ import SiteLayout from "../SiteLayout";
 import StoryNav from "./StoryNav";
 import StoryList from "./StoryList";
 
-const StoryView = ({ useHook, activeRoute }) => {
+const StoryView = ({ useHook }) => {
 
   return ( 
     <SiteLayout contentClassName="grid-rows-[auto,1fr,auto]">
