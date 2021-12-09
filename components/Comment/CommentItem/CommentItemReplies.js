df09be95e99d2fee91cf88e26c926f8f8ce677c6
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-09 14:16:41 -0700

    change comment reply generation logic with limits
    add limits

--- components/Comment/CommentItem/CommentItemReplies.js
+++ components/Comment/CommentItem/CommentItemReplies.js
@@ -3,31 +3,57 @@ import { useState } from "react";
 import CommentItem from "./CommentItem";
 
 const CommentItemReplies = ({ replyIds, replyDepthLimit, parentDepth }) => {
-  const [isLoadedManually, setIsLoadedManually] = useState(false);
   const currentDepth = parentDepth + 1;
+  const defaultReplyCount = 3;
+  const replyIncrementCount = 5;
+  const [replyCount, setReplyCount] = useState(defaultReplyCount);
+  const [isChildrenLoaded, setIsChildrenLoaded] = useState(false);
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!isChildrenLoaded) {
+      setIsChildrenLoaded(true);
+    } else {
+      const newCount = replyCount + replyIncrementCount;
+      setReplyCount(newCount);
+    }
+  }
+
+  // automatically display component until the set replyDepthLimit has been reached.
+  // if replyDepthLimit has been reached, a trigger will be displayed to manually load the replies with the provided replyIds
+  // a trigger will also be displayed if not all children are loaded
   return !replyIds ? null : replyIds.length <= 0 ? null : (
-    (currentDepth < replyDepthLimit) || isLoadedManually ? (
-      <div className="grid gap-4 border-l-brandDefault border-brandButtonOutline pl-4 mt-4">
-        { replyIds.map((replyId) => (
-          <CommentItem 
-            key={replyId}
-            commentId={replyId} 
-            replyDepthLimit={replyDepthLimit}
-            parentDepth={currentDepth}
-          />
-        ))}
-      </div>
-    ) : (
-      <div className="grid place-items-start border-l-brandDefault border-brandButtonOutline mt-2 px-4">
-        <button 
-          className="font-bold text-xs text-brandButtonInlineText tracking-wide text-left hover:underline sm:text-brandTextPrimary sm:tracking-normal"
-          onClick={() => setIsLoadedManually(true)}
-        >
-          { replyIds.length } more repl{ replyIds.length > 1 ? "ies" : "y" }
-        </button>
-      </div>
-    )
+    <div className="grid gap-4 border-l-brandDefault border-brandButtonOutline mt-4 pl-4">
+      {/* comment replies */}
+      { (currentDepth < replyDepthLimit || isChildrenLoaded) && (
+        <div className="grid gap-4">
+          { [...replyIds].slice(0, replyCount).map((replyId) => (
+            <CommentItem 
+              key={replyId}
+              commentId={replyId} 
+              replyDepthLimit={replyDepthLimit}
+              parentDepth={currentDepth}
+            />
+          ))}
+        </div>
+      )}
+
+      {/* display load more replies button if: a) no children has been loaded, b) not all child replies are loaded */}
+      { ((currentDepth >= replyDepthLimit && !isChildrenLoaded) || replyIds.length > replyCount) && (
+        <div className="grid place-items-start">
+          <button 
+            className="font-bold text-xs text-brandButtonInlineText tracking-wide text-left hover:underline sm:text-brandTextPrimary sm:tracking-normal"
+            onClick={(e) => handleClick(e)}
+          >
+            { !isChildrenLoaded ? (
+              <span>{ replyIds.length } more repl{ replyIds.length > 1 ? "ies" : "y" }</span>
+            ) : (
+              <span>{ replyIds.length-replyCount } more repl{ replyIds.length-replyCount > 1 ? "ies" : "y" }</span>
+            )}
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
  
