7cf213e3fc92f643f1b9aa306bd5f268032d8692
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-09 14:49:08 -0700

    create function for similar statements

--- components/Comment/CommentItem/CommentItemReplies.js
+++ components/Comment/CommentItem/CommentItemReplies.js
@@ -9,23 +9,28 @@ const CommentItemReplies = ({ replyIds, replyDepthLimit, parentDepth }) => {
   const [replyCount, setReplyCount] = useState(defaultReplyCount);
   const [isChildrenLoaded, setIsChildrenLoaded] = useState(false);
 
+  const isReplyDepthLimitReached = () => {
+    return (currentDepth >= replyDepthLimit) ? true : false;
+  }
+
   const handleClick = (e) => {
     e.preventDefault();
-    if (!isChildrenLoaded) {
+    if (isReplyDepthLimitReached() && !isChildrenLoaded) {
       setIsChildrenLoaded(true);
-    } else {
+    } 
+    else {
       const newCount = replyCount + replyIncrementCount;
       setReplyCount(newCount);
     }
   }
 
   // automatically display component until the set replyDepthLimit has been reached.
-  // if replyDepthLimit has been reached, a trigger will be displayed to manually load the replies with the provided replyIds
-  // a trigger will also be displayed if not all children are loaded
+  // if replyDepthLimit has been reached, a trigger will be displayed to manually load the replies with the provided replyIds.
+  // a trigger will also be displayed if not all children are loaded.
   return !replyIds ? null : replyIds.length <= 0 ? null : (
     <div className="grid gap-4 border-l-brandDefault border-brandButtonOutline mt-4 pl-4">
       {/* comment replies */}
-      { (currentDepth < replyDepthLimit || isChildrenLoaded) && (
+      { (!isReplyDepthLimitReached() || isChildrenLoaded) && (
         <div className="grid gap-4">
           { [...replyIds].slice(0, replyCount).map((replyId) => (
             <CommentItem 
@@ -39,13 +44,13 @@ const CommentItemReplies = ({ replyIds, replyDepthLimit, parentDepth }) => {
       )}
 
       {/* display load more replies button if: a) no children has been loaded, b) not all child replies are loaded */}
-      { ((currentDepth >= replyDepthLimit && !isChildrenLoaded) || replyIds.length > replyCount) && (
+      { ((isReplyDepthLimitReached() && !isChildrenLoaded) || replyIds.length > replyCount) && (
         <div className="grid place-items-start">
           <button 
             className="font-bold text-xs text-brandButtonInlineText tracking-wide text-left hover:underline sm:text-brandTextPrimary sm:tracking-normal"
             onClick={(e) => handleClick(e)}
           >
-            { !isChildrenLoaded ? (
+            { isReplyDepthLimitReached() ? (
               <span>{ replyIds.length } more repl{ replyIds.length > 1 ? "ies" : "y" }</span>
             ) : (
               <span>{ replyIds.length-replyCount } more repl{ replyIds.length-replyCount > 1 ? "ies" : "y" }</span>
