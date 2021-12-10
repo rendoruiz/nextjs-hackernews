f0d9d8efc51d67aed56e93d143362543541a38d7
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-10 12:00:56 -0700

    comment item desktop loader

--- components/Comment/CommentItem/CommentItem.js
+++ components/Comment/CommentItem/CommentItem.js
@@ -17,7 +17,7 @@ const CommentItem = ({ commentId, submitterId, replyDepthLimit, parentDepth = 0 
     setIsCollapsed(!isCollapsed);
   }
 
-  return isLoading ? (<IsLoading itemDepth={parentDepth} />) : isError || !data ? (<ItemIsError error={error} />) : isSuccess && (
+  return isSuccess ? (<IsLoading itemDepth={parentDepth} />) : isError || !data ? (<ItemIsError error={error} />) : isSuccess && (
     !data.deleted && (  
       <div className={clsx(
         "grid text-sm sm:grid-cols-[auto,1fr] sm:gap-x-2 sm:border-none sm:p-0",
@@ -88,24 +88,26 @@ const CommentItem = ({ commentId, submitterId, replyDepthLimit, parentDepth = 0 
 const IsLoading = ({ itemDepth }) => {
   return (
     <div className={clsx(
-      "grid gap-1 text-sm sm:border-none sm:px-0",
+      "grid text-sm sm:grid-cols-[auto,1fr] sm:gap-x-2 sm:gap-y-1 sm:border-none sm:p-0",
       { "border-t-brandDefault border-t-brandButtonOutline px-4 pt-3 first:border-t-0 first:pt-0": itemDepth === 0 },
     )}>
-      <div className="grid grid-cols-[auto,1fr] gap-2">
+      <div className="grid grid-cols-[auto,1fr] gap-2 items-center text-xs sm:col-span-2 sm:auto-cols-auto">
         <div className={clsx(
-          "rounded-full bg-brandTextSecondary/30 animate-pulse", 
+          "rounded-full bg-brandTextSecondary/30 animate-pulse sm:w-7 sm:h-7", 
           itemDepth === 0 ? "w-6 h-6" : "w-[18px] h-[18px]"
           )}/>
         <div className="flex items-center">
-          <div className="rounded-md w-10 h-3 bg-brandTextSecondary/30 animate-pulse" />
-          <div className="rounded-md ml-2 w-5 h-3 bg-brandTextSecondary/30 animate-pulse" />
+          <div className="rounded-md w-16 h-3 bg-brandTextSecondary/30 animate-pulse" />
+          <div className="rounded-md ml-2 w-5 h-3 bg-brandTextSecondary/30 animate-pulse sm:w-24" />
         </div>
       </div>
-      
-      <div className={clsx("grid", itemDepth === 0 ? "ml-8" : "mt-[0.125rem]")}>
-        <div className="grid gap-[2px]">
+
+      <div className="hidden sm:grid col-start-1 w-7 overflow-hidden"></div>
+
+      <div className={clsx("grid sm:col-start-2 sm:ml-0 sm:mt-0", itemDepth === 0 ? "ml-8" : "mt-[0.125rem]")}>
+        <div className="grid gap-[2px] sm:gap-1">
           <div className="rounded-md w-10/12 h-4 bg-brandTextSecondary/30 animate-pulse" />
-          <div className="rounded-md w-full h-4 bg-brandTextSecondary/30 animate-pulse" />
+          <div className="rounded-md w-full h-4 bg-brandTextSecondary/30 animate-pulse sm:w-11/12" />
           <div className="rounded-md w-3/4 h-4 bg-brandTextSecondary/30 animate-pulse" />
         </div>
       </div>
