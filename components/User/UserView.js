03f040f33b045d5160b532a1b2888e57cb992f34
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-20 16:52:07 -0700

    align component layout with other views

--- components/User/UserView.js
+++ components/User/UserView.js
@@ -1,23 +1,31 @@
 import { useRouter } from "next/dist/client/router";
+
 import { useUser } from "../../hooks/useUser";
 import SiteLayout from "../SiteLayout";
 import IsError from "../StatusMessage/IsError";
 import IsLoading from "../StatusMessage/IsLoading";
 import UserDetails from "./UserDetails/UserDetails";
-import UserContent from "./UserContent/UserContent";
+import UserContentNavigationBar from "./UserContent/UserContentNavigationBar";
+import UserContentList from "./UserContent/UserContentList";
 
 const UserView = () => {
   const router = useRouter();
   const { userid } = router.query;
-  const { isLoading, isError, data, isSuccess } = useUser(userid);
+  const { isLoading, isError, data: userData, isSuccess } = useUser(userid);
 
   return userid && isLoading ? (<IsLoading />) : isError ? (<IsError />) : isSuccess && (  
     <SiteLayout contentClassName="md:grid-cols-[1fr,auto] md:gap-x-6">
       {/* details header (mobile)/sidebar (desktop) */}
-      <UserDetails userData={data} />
+      <UserDetails userData={userData} />
 
-      {/* user submitted contents */}
-      <UserContent userData={data} />
+      {/* user submitted contents + filter nav */}
+      <div className="grid grid-rows-[auto,1fr]">
+        <UserContentNavigationBar userId={userData.id} />
+        <UserContentList
+          contentIds={userData.submitted}
+          userId={userData.id}
+        />
+      </div>
     </SiteLayout>
   );
 }
