e4b281176a08c46dd3416588c22a1c049118d2cb
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-19 18:34:59 -0700

    content item logic overhaul

--- components/User/UserContent/UserContentItem.js
+++ components/User/UserContent/UserContentItem.js
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
 import { useComment } from "../../../hooks/useComment";
 import StoryItem from "../../Story/StoryItem/StoryItem";
 
-const UserContentItem = ({ contentId }) => {
+const UserContentItem = ({ contentId, restrictedContent }) => {
   const { data, isSuccess } = useComment(contentId);
+  const [contentItem, setContentItem] = useState(null);
 
-  const setContent = (contentType, contentId) => {
-    if (contentType === "comment") {
-      return (
+  const setContent = () => {
+    if (data.type === "comment" && restrictedContent !== "comment") {
+      setContentItem(
         <div>this is a comment</div>
-      )
+      );
+    } else if (data.type !== "comment" && restrictedContent !== "story") {
+      setContentItem(
+        <StoryItem storyId={contentId} />
+      );
     } else {
-      return (<StoryItem storyId={contentId} />)
+      setContentItem(null);
     }
   }
 
-  return isSuccess && data && setContent(data.type, data.id);
+  // run once - when content item is still not been set
+  useEffect(() => {
+    if (data && !contentItem) {
+      setContent();
+    }
+  }, [data]);
+
+  // run everytime - when restrictedContent has changed only if content item has already been set.
+  useEffect(() => {
+    if (contentItem) {
+      setContent();
+    }
+  }, [restrictedContent]);
+
+  return isSuccess && data && contentItem;
 }
  
 export default UserContentItem;
