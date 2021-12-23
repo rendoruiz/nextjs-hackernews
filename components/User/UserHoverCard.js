784249030ee75450fab19d20619c454a9a1c0925
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-23 16:29:59 -0700

    Update UserHoverCard.js

--- components/User/UserHoverCard.js
+++ components/User/UserHoverCard.js
@@ -30,7 +30,7 @@ const UserHoverCard = ({ userId, className, withPrefix, withAvatar, avatarClassN
         </div>
       </HoverCard.Trigger>
       <HoverCard.Content 
-        className="grid gap-4 border-brandDefault border-brandBorder rounded p-3 pb-4 bg-white shadow-md"
+        className="grid gap-4 border-brandDefault border-brandBorder rounded p-3 pb-4 bg-brandObjectBackground shadow-md dark:border-brandDarkBorder dark:bg-brandDarkObjectBackground dark:shadow-transientObjectDark"
         sideOffset={2}
         align="start"
       >
@@ -70,14 +70,13 @@ const CardContent = ({ userId, isOpen }) => {
         className="w-9 h-9 text-2xl"
         userId={data.id}
       />
-      {/* <div className="rounded bg-brandOrange "></div> */}
       <div className="grid">
         <UserLink
-          className="font-medium text-lg text-brandTextPrimary"
+          className="font-medium text-lg text-brandTextPrimary dark:text-brandDarkTextPrimary"
           userId={data.id}
           withPrefix
         />
-        <p className="text-xs text-brandTextSecondary leading-tight tracking-wide">
+        <p className="text-xs text-brandTextSecondary leading-tight tracking-wide dark:text-brandDarkTextSecondary">
           { data.id } • { relativeTime }
         </p>
       </div>
@@ -87,19 +86,19 @@ const CardContent = ({ userId, isOpen }) => {
     <section className="grid grid-cols-2 gap-5">
       {/* score */}
       <div className="grid">
-        <p className="order-2 text-xs text-brandTextSecondary leading-snug tracking-wide">
+        <p className="order-2 text-xs text-brandTextSecondary leading-snug tracking-wide dark:text-brandDarkTextSecondary">
           Karma
         </p>
-        <p className="order-1 font-medium text-lg text-brandTextPrimary leading-snug">
+        <p className="order-1 font-medium text-lg text-brandTextPrimary leading-snug dark:text-brandDarkTextPrimary">
           { karma }
         </p>
       </div>
       {/* creation date */}
       <div className="grid">
-        <p className="order-2 text-xs text-brandTextSecondary leading-snug tracking-wide">
+        <p className="order-2 text-xs text-brandTextSecondary leading-snug tracking-wide dark:text-brandDarkTextSecondary">
           Submissions
         </p>
-        <p className="order-1 font-medium text-lg text-brandTextPrimary leading-snug">
+        <p className="order-1 font-medium text-lg text-brandTextPrimary leading-snug dark:text-brandDarkTextPrimary">
           { submissions }
         </p>
       </div>
