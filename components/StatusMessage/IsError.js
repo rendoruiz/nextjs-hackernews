b9d437e97cc6346fe5fa1aa350711bdc2e8c1c77
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2021-12-24 18:27:08 -0700

    back button on error

--- components/StatusMessage/IsError.js
+++ components/StatusMessage/IsError.js
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 // fetch error
 const IsError = () => {
+  const router = useRouter(); 
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.back();
+  }
+
   return (  
     <div className="grid place-items-center px-5 py-10 only:row-span-2">
       <p className="grid gap-5 text-center sm:gap-8 lg:gap-10">
@@ -12,6 +22,13 @@ const IsError = () => {
         <span className="font-medium text-2xl text-brandTextPrimary dark:text-brandDarkTextPrimary sm:text-3xl lg:text-4xl">
           An error occured.
         </span>
+
+        <button 
+          className="justify-self-center rounded-full mt-5 px-7 py-[6px] bg-brandOrange font-medium text-xs text-white uppercase tracking-wider transition hover:opacity-80 active:opacity-60 dark:bg-brandAppBackground dark:text-brandTextPrimary sm:px-10 sm:py-2 sm:text-sm"
+          onClick={(e) => handleClick(e)}
+        >
+          Go back
+        </button>
       </p>
     </div>
   );
